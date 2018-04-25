53bdbe1b2519a8957f153f8dbe1e2ec6e9753869
Nikos Katsarakis <nkatsar@gmail.com>
2018-04-25 23:53:05 +0300

    Update Spring Boot to 2.0.1

--- src/main/java/org/paginationdemo/domain/Person.java
+++ src/main/java/org/paginationdemo/domain/Person.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long personId;
 
     private String firstName;
